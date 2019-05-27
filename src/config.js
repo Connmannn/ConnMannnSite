6bd906c6f164dbc9040a469de000f3b6308ca339
Connmannn <matthew.t.connaughton@gmail.com>
2019-05-26 19:41:48 -0500

    First commit

---
+++ src/config.js
@@ -0,0 +1,18 @@
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "connmannn"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://zh9r71gc0b.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_lCAqhMNA6",
+      APP_CLIENT_ID: "5ovuniuk9jmo19da3nomhn3p1f",
+      IDENTITY_POOL_ID: "us-east-2:d41098a7-f26e-4fb6-86f8-3a4f0ba9f795"
+    }
+  };
+  
